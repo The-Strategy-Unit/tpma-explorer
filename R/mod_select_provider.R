47b05e86c1312579cb94e8979738acee5a70c6c8
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2025-12-02 10:25:44 +0000

    Introduce logic on the basis of geography

--- R/mod_select_provider.R
+++ R/mod_select_provider.R
@@ -3,17 +3,10 @@
 #' @noRd
 mod_select_provider_ui <- function(id) {
   ns <- shiny::NS(id)
-  shiny::tagList(
-    shiny::selectInput(
-      ns("geography_select"),
-      "Choose a geography:",
-      choices = NULL
-    ),
-    shiny::selectInput(
-      ns("provider_select"),
-      "Choose a provider:",
-      choices = NULL
-    )
+  shiny::selectInput(
+    ns("provider_select"),
+    "Choose a provider:",
+    choices = NULL
   )
 }
 
@@ -25,22 +18,13 @@ mod_select_provider_ui <- function(id) {
 #'     their values are the corresponding human-readable names and codes (e.g.
 #'     `"Airedale NHS Foundation Trust (RCF)"`).
 #' @noRd
-mod_select_provider_server <- function(id, geographies, providers) {
+mod_select_provider_server <- function(id, selected_geography, providers) {
   shiny::moduleServer(id, function(input, output, session) {
-    shiny::observe({
-      shiny::req(geographies)
-      shiny::updateSelectInput(
-        session,
-        "geography_select",
-        choices = geographies
-      )
-    })
-    selected_geography <- shiny::reactive(input$geography_select)
-
     shiny::observe({
       shiny::req(selected_geography())
       shiny::req(providers)
-      if (selected_geography() == "NHP schemes") {
+
+      if (selected_geography() == "nhp") {
         provider_choices <- purrr::set_names(names(providers), providers)
         shiny::updateSelectInput(
           session,
@@ -49,7 +33,17 @@ mod_select_provider_server <- function(id, geographies, providers) {
           choices = provider_choices
         )
       }
+      # TODO: correct logic when LA is selected
+      if (selected_geography() == "la") {
+        shiny::updateSelectInput(
+          session,
+          "provider_select",
+          label = "Choose a local authority:",
+          choices = NULL
+        )
+      }
     })
+
     shiny::reactive(input$provider_select)
   })
 }
