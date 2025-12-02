632dbe9826ebe5b57e3f3875879502af11afe841
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2025-12-02 10:22:42 +0000

    Add geography selection module, pass to server/UI

--- R/app_server.R
+++ R/app_server.R
@@ -2,73 +2,82 @@
 #' @param input,output,session Internal parameters for 'shiny'.
 #' @noRd
 app_server <- function(input, output, session) {
-  # Env variables ----
-  inputs_container_name <- Sys.getenv("AZ_CONTAINER_INPUTS")
-  data_version <- Sys.getenv("DATA_VERSION")
+  # Variables ----
+  geographies <- c("nhp", "la")
   baseline_year <- Sys.getenv("BASELINE_YEAR") |> as.numeric()
 
   # Data ----
-  inputs_container <- get_container(container_name = inputs_container_name)
-  rates_data <- azkit::read_azure_parquet(
-    inputs_container,
-    "rates",
-    data_version
+
+  inputs_container <- get_container(
+    container_name = Sys.getenv("AZ_CONTAINER_INPUTS")
   )
-  procedures_data <- azkit::read_azure_parquet(
+  inputs_data <- get_all_geo_data(
     inputs_container,
-    "procedures",
-    data_version
+    geographies,
+    data_type = c("age_sex", "diagnoses", "procedures", "rates")
   )
-  diagnoses_data <- azkit::read_azure_parquet(
-    inputs_container,
-    "diagnoses",
-    data_version
-  )
-  age_sex_data <- azkit::read_azure_parquet(
-    inputs_container,
-    "age_sex",
-    data_version
-  ) |>
-    prepare_age_sex_data()
+  age_sex_data <- shiny::reactive({
+    shiny::req(selected_geography())
+    inputs_data[[selected_geography()]][["age_sex"]] |>
+      prepare_age_sex_data()
+  })
+  diagnoses_data <- shiny::reactive({
+    shiny::req(selected_geography())
+    inputs_data[[selected_geography()]][["diagnoses"]]
+  })
+  procedures_data <- shiny::reactive({
+    shiny::req(selected_geography())
+    inputs_data[[selected_geography()]][["procedures"]]
+  })
+  rates_data <- shiny::reactive({
+    shiny::req(selected_geography())
+    inputs_data[[selected_geography()]][["rates"]]
+  })
+
   nee_data <- readr::read_csv(
     app_sys("app", "data", "nee_table.csv"),
     col_types = "cddd"
   )
 
-  # Lookups ----
-  providers_lookup <- jsonlite::read_json(
-    app_sys("app", "data", "datasets.json"),
-    simplify_vector = TRUE
-  )
-  strategies_lookup <- jsonlite::read_json(
-    app_sys("app", "data", "mitigators.json"),
-    simplify_vector = TRUE
-  )
+  # Lookups (general) ----
   descriptions_lookup <- jsonlite::read_json(
     app_sys("app", "data", "descriptions.json"),
     simplifyVector = TRUE
   )
-  peers_lookup <- readr::read_csv(
-    app_sys("app", "data", "peers.csv"),
+  diagnoses_lookup <- readr::read_csv(
+    app_sys("app", "data", "diagnoses.csv"),
     col_types = "c"
   )
   procedures_lookup <- readr::read_csv(
     app_sys("app", "data", "procedures.csv"),
     col_types = "c"
   )
-  diagnoses_lookup <- readr::read_csv(
-    app_sys("app", "data", "diagnoses.csv"),
-    col_types = "c"
+  strategies_config <- get_golem_config("mitigators_config")
+  strategies_lookup <- jsonlite::read_json(
+    app_sys("app", "data", "mitigators.json"),
+    simplify_vector = TRUE
   )
 
-  # Config ----
-  strategies_config <- get_golem_config("mitigators_config")
-  geographies <- c("NHP schemes", "Local Authorities")
+  # Lookups ----
+  # TODO: conditionally select based on geography
+  # TODO: add files for LA versions of these files
+  peers_lookup <- readr::read_csv(
+    app_sys("app", "data", "peers.csv"),
+    col_types = "c"
+  )
+  providers_lookup <- jsonlite::read_json(
+    app_sys("app", "data", "datasets.json"),
+    simplify_vector = TRUE
+  )
 
   # User inputs ----
+  selected_geography <- mod_select_geography_server(
+    "mod_select_geography",
+    geographies
+  )
   selected_provider <- mod_select_provider_server(
     "mod_select_provider",
-    geographies,
+    selected_geography,
     providers_lookup
   )
   selected_strategy <- mod_select_strategy_server(
