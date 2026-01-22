5df68b0479ebbae2a22b3de77b200fe924b46e22
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2026-01-22 15:11:33 +0000

    Use page_navbar layout, add link to feedback

--- R/app_ui.R
+++ R/app_ui.R
@@ -2,9 +2,60 @@
 #' @param request Internal parameter for 'shiny'.
 #' @noRd
 app_ui <- function(request) {
-  bslib::page_sidebar(
+  bslib::page_navbar(
+    id = "page_navbar",
     title = "Explore potentially-mitigatable activity data (in development)",
+    selected = "Data", # start with this panel open
     fillable = FALSE, # allow page scroll
+
+    bslib::nav_panel(
+      id = "nav_panel_data",
+      title = "Data",
+
+      bslib::card(
+        fill = FALSE,
+        bslib::card_header(
+          class = "bg-warning",
+          bsicons::bs_icon("exclamation-triangle"),
+          "Warning"
+        ),
+        "This application is in development and its output has not been verified.
+      The information presented here should not be relied on as fact."
+      ),
+
+      bslib::layout_columns(
+        col_widths = c(3, 9),
+        fill = FALSE,
+        fillable = FALSE,
+        mod_show_strategy_text_ui("mod_show_strategy_text"),
+        bslib::layout_column_wrap(
+          width = 1,
+          gap = "0.8rem",
+          mod_plot_rates_ui("mod_plot_rates"),
+          bslib::layout_column_wrap(
+            width = 1 / 2,
+            mod_table_procedures_ui("mod_table_procedures"),
+            mod_table_diagnoses_ui("mod_table_diagnoses")
+          ),
+          bslib::layout_column_wrap(
+            width = 1 / 2,
+            mod_plot_age_sex_pyramid_ui("mod_plot_age_sex_pyramid"),
+            mod_plot_nee_ui("mod_plot_nee")
+          ),
+        )
+      )
+    ),
+
+    bslib::nav_item(
+      class = "ms-auto", # push to far-right
+      tags$a(
+        href = Sys.getenv("FEEDBACK_FORM_URL"),
+        target = "_blank",
+        class = "nav-link",
+        shiny::icon("comment-dots"),
+        "Give feedback"
+      )
+    ),
 
     sidebar = bslib::sidebar(
       bslib::accordion(
@@ -22,39 +73,6 @@ app_ui <- function(request) {
           icon = bsicons::bs_icon("hospital"),
           mod_select_strategy_ui("mod_select_strategy")
         )
-      )
-    ),
-
-    bslib::card(
-      fill = FALSE,
-      bslib::card_header(
-        class = "bg-warning",
-        bsicons::bs_icon("exclamation-triangle"),
-        "Warning"
-      ),
-      "This application is in development and its output has not been verified.
-      The information presented here should not be relied on as fact."
-    ),
-
-    bslib::layout_columns(
-      col_widths = c(3, 9),
-      fill = FALSE,
-      fillable = FALSE,
-      mod_show_strategy_text_ui("mod_show_strategy_text"),
-      bslib::layout_column_wrap(
-        width = 1,
-        gap = "0.8rem",
-        mod_plot_rates_ui("mod_plot_rates"),
-        bslib::layout_column_wrap(
-          width = 1 / 2,
-          mod_table_procedures_ui("mod_table_procedures"),
-          mod_table_diagnoses_ui("mod_table_diagnoses")
-        ),
-        bslib::layout_column_wrap(
-          width = 1 / 2,
-          mod_plot_age_sex_pyramid_ui("mod_plot_age_sex_pyramid"),
-          mod_plot_nee_ui("mod_plot_nee")
-        ),
       )
     )
   )
