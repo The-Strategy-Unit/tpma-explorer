31a8b62c416da98dd20a3eb9927def3d8929afd2
Tom Jemmett <thomas.jemmett@nhs.net>
2026-02-03 12:41:24 +0000

    separates app_ui tests

--- tests/testthat/test-app_ui.R
+++ tests/testthat/test-app_ui.R
@@ -1,9 +1,10 @@
 library(mockery)
 
-test_that("ui", {
+setup_app_ui_tests <- function(.env = parent.frame()) {
   testthat::local_mocked_bindings(
     "p_randomInt" = \(...) "X",
-    .package = "shiny"
+    .package = "shiny",
+    .env = .env
   )
 
   mocks <- list(
@@ -18,36 +19,128 @@ test_that("ui", {
     "mod_select_strategy_ui" = mock("mod_select_strategy")
   )
 
-  do.call(testthat::local_mocked_bindings, c(mocks))
+  do.call(testthat::local_mocked_bindings, c(mocks, .env = .env))
 
+  mocks
+}
+
+test_that("ui", {
+  # arrange
+  setup_app_ui_tests()
+
+  # act
   ui <- app_ui("request")
 
+  # assert
   expect_snapshot(ui)
+})
 
+test_that("calls mod_show_strategy_text_ui", {
+  # arrange
+  mocks <- setup_app_ui_tests()
+
+  # act
+  ui <- app_ui("request")
+
+  # assert
   expect_called(mocks$mod_show_strategy_text_ui, 1)
   expect_args(mocks$mod_show_strategy_text_ui, 1, "mod_show_strategy_text")
+})
 
+test_that("calls mod_plot_rates_ui", {
+  # arrange
+  mocks <- setup_app_ui_tests()
+
+  # act
+  ui <- app_ui("request")
+
+  # assert
   expect_called(mocks$mod_plot_rates_ui, 1)
   expect_args(mocks$mod_plot_rates_ui, 1, "mod_plot_rates")
+})
 
+test_that("calls mod_table_procedures_ui", {
+  # arrange
+  mocks <- setup_app_ui_tests()
+
+  # act
+  ui <- app_ui("request")
+
+  # assert
   expect_called(mocks$mod_table_procedures_ui, 1)
   expect_args(mocks$mod_table_procedures_ui, 1, "mod_table_procedures")
+})
 
+
+test_that("calls mod_table_diagnoses_ui", {
+  # arrange
+  mocks <- setup_app_ui_tests()
+
+  # act
+  ui <- app_ui("request")
+
+  # assert
   expect_called(mocks$mod_table_diagnoses_ui, 1)
   expect_args(mocks$mod_table_diagnoses_ui, 1, "mod_table_diagnoses")
+})
 
+
+test_that("calls mod_plot_age_sex_pyramid", {
+  # arrange
+  mocks <- setup_app_ui_tests()
+
+  # act
+  ui <- app_ui("request")
+
+  # assert
   expect_called(mocks$mod_plot_age_sex_pyramid_ui, 1)
   expect_args(mocks$mod_plot_age_sex_pyramid_ui, 1, "mod_plot_age_sex_pyramid")
+})
 
+test_that("calls mod_plot_nee_ui", {
+  # arrange
+  mocks <- setup_app_ui_tests()
+
+  # act
+  ui <- app_ui("request")
+
+  # assert
   expect_called(mocks$mod_plot_nee_ui, 1)
   expect_args(mocks$mod_plot_nee_ui, 1, "mod_plot_nee")
+})
 
+test_that("calls mod_select_geography_ui", {
+  # arrange
+  mocks <- setup_app_ui_tests()
+
+  # act
+  ui <- app_ui("request")
+
+  # assert
   expect_called(mocks$mod_select_geography_ui, 1)
   expect_args(mocks$mod_select_geography_ui, 1, "mod_select_geography")
+})
 
+test_that("calls mod_select_provider_ui", {
+  # arrange
+  mocks <- setup_app_ui_tests()
+
+  # act
+  ui <- app_ui("request")
+
+  # assert
   expect_called(mocks$mod_select_provider_ui, 1)
   expect_args(mocks$mod_select_provider_ui, 1, "mod_select_provider")
+})
 
+test_that("calls mod_select_strategy_ui", {
+  # arrange
+  mocks <- setup_app_ui_tests()
+
+  # act
+  ui <- app_ui("request")
+
+  # assert
   expect_called(mocks$mod_select_strategy_ui, 1)
   expect_args(mocks$mod_select_strategy_ui, 1, "mod_select_strategy")
 })
