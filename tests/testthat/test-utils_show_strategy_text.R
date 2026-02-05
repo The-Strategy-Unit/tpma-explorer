fe836c133e259959dce95ea6395bc776fd3c3f34
Tom Jemmett <thomas.jemmett@nhs.net>
2026-02-05 11:42:25 +0000

    improves fetch_strategy_text tests

--- tests/testthat/test-utils_show_strategy_text.R
+++ tests/testthat/test-utils_show_strategy_text.R
@@ -11,3 +11,22 @@ test_that("fetch_strategy_text works", {
   # assert
   expect_equal(text, "strategy text")
 })
+
+test_that("fetch_strategy_text calls correct url", {
+  # arrange
+  local_mocked_bindings(
+    "req_perform" = identity,
+    "resp_body_string" = identity,
+    .package = "httr2"
+  )
+
+  # act
+  req <- fetch_strategy_text("stub")
+
+  # assert
+
+  expect_equal(
+    req$url,
+    "https://raw.githubusercontent.com/The-Strategy-Unit/nhp_inputs/refs/heads/main/inst/app/strategy_text/stub.md" # nolint
+  )
+})
