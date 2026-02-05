2a4a5ba6640c936d891b37dd1e95f1f0cfb1a95e
Tom Jemmett <thomas.jemmett@nhs.net>
2026-02-05 09:27:11 +0000

    adds tests for entable_encounters

---
+++ tests/testthat/test-fct_table.R
@@ -0,0 +1,35 @@
+test_that("entable_encounters (diagnoses)", {
+  # arrange
+  set.seed(1)
+  df <- tibble::tribble(
+    ~diagnosis_description, ~n, ~pcnt,
+    "A", 100, 0.5,
+    "B", 50, 0.25,
+    "C", 25, 0.125,
+    "Other", 25, 0.125
+  )
+
+  # act
+  actual <- entable_encounters(df)
+
+  # assert
+  expect_snapshot(actual)
+})
+
+test_that("entable_encounters (procedures)", {
+  # arrange
+  set.seed(1)
+  df <- tibble::tribble(
+    ~procedures_description, ~n, ~pcnt,
+    "A", 100, 0.5,
+    "B", 50, 0.25,
+    "C", 25, 0.125,
+    "Other", 25, 0.125
+  )
+
+  # act
+  actual <- entable_encounters(df)
+
+  # assert
+  expect_snapshot(actual)
+})
