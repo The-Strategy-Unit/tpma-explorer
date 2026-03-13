d314edd9d6c9f672f47d5568a9a3fb1b3ace207f
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2026-03-13 12:24:38 +0000

    Update deploy script with dev version

--- dev/deploy.R
+++ dev/deploy.R
@@ -1,15 +1,38 @@
-rsconnect::deployApp(
-  appName = "tpma-explorer",
-  appTitle = "Explore TPMA data",
-  server = "connect.strategyunitwm.nhs.uk",
-  appId = 200,
-  appFiles = c(
-    "R",
-    "inst",
-    "NAMESPACE",
-    "DESCRIPTION",
-    "app.R"
-  ),
-  lint = FALSE,
-  forceUpdate = TRUE
-)
+deploy <- function(type = c("dev", "prod")) {
+  type <- match.arg(type)
+
+  # prod details
+  app_id <- 200
+  app_name <- "tpma-explorer"
+  app_title <- "TPMA explorer"
+
+  if (type == "dev") {
+    app_id <- 304
+    app_name <- paste0(app_name, "-dev")
+    app_title <- paste(app_title, "(dev)")
+  }
+
+  rsconnect::deployApp(
+    appName = app_name,
+    appTitle = app_title,
+    server = "connect.strategyunitwm.nhs.uk",
+    appId = app_id,
+    appFiles = c(
+      "R",
+      "inst",
+      "NAMESPACE",
+      "DESCRIPTION",
+      "app.R"
+    ),
+    lint = FALSE,
+    forceUpdate = TRUE
+  )
+}
+
+# Deploy development version between releases to
+# https://connect.strategyunitwm.nhs.uk/tpma-explorer-dev/
+deploy(type = "dev")
+
+# Deploy on release to
+# https://connect.strategyunitwm.nhs.uk/tpma-explorer/
+deploy(type = "prod")
