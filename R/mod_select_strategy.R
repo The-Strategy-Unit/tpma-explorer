8700e8f61d382b388d0b75a5023329101709dd16
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2026-03-18 10:15:24 +0000

    Introduce category dropdown

--- R/mod_select_strategy.R
+++ R/mod_select_strategy.R
@@ -5,17 +5,6 @@ mod_select_strategy_ui <- function(id) {
   ns <- shiny::NS(id)
   shiny::tagList(
     shiny::selectInput(
-      ns("strategy_care_shift_select"),
-      label = bslib::tooltip(
-        trigger = list(
-          "Choose a care-shift category",
-          bsicons::bs_icon("info-circle")
-        ),
-        md_file_to_html("app", "text", "sidebar-tooltip-selections.md"),
-      ),
-      choices = NULL
-    ),
-    shiny::selectInput(
       ns("strategy_activity_type_select"),
       label = bslib::tooltip(
         trigger = list(
@@ -25,10 +14,22 @@ mod_select_strategy_ui <- function(id) {
         md_file_to_html("app", "text", "sidebar-tooltip-selections.md"),
       ),
       choices = c(
+        # TODO: set this in the server for consistency with other dropdowns?
         "Inpatients" = "ip",
         "Outpatients" = "op",
         "Accident & Emergency" = "ae"
       )
+    ),
+    shiny::selectInput(
+      ns("strategy_category_select"),
+      label = bslib::tooltip(
+        trigger = list(
+          "Filter by category",
+          bsicons::bs_icon("info-circle")
+        ),
+        md_file_to_html("app", "text", "sidebar-tooltip-selections.md"),
+      ),
+      choices = NULL
     ),
     shiny::selectInput(
       ns("strategy_select"),
@@ -41,28 +42,44 @@ mod_select_strategy_ui <- function(id) {
   )
 }
 
-#' Get TPMAs for the drop down menu
+#' Prepare Table of Options for Drodown Menus
 #'
-#' Reads the mitigators.json file and extracts the name and category (IP/OP/AE).
+#' Reads the local mitigator-categories.csv and mitigators.json files. Extracts
+#' names and categories into a single lookup table that can be filtered to help
+#' decide what values to put in the dropdown menus.
 #'
-#' @return A named list of data frames, where the names are the categories (IP/OP/AE)
+#' @return A data.frame.
 #' @noRd
 mod_select_strategy_get_strategies <- function() {
+  # Read local lookups
+  categories <- app_sys("app", "data", "mitigator-categories.csv") |>
+    readr::read_csv(col_types = "c")
   strategies <- app_sys("app", "data", "mitigators.json") |>
     yyjsonr::read_json_file()
 
   strategies |>
     unlist() |>
-    tibble::enframe("strategy", "name") |>
+    tibble::enframe("strategy", "strategy_name") |>
     dplyr::mutate(
-      category = stringr::str_extract(
-        .data$name,
+      activity_type = stringr::str_extract(
+        .data$strategy_name,
         "(?<= \\()(IP|OP|AE)(?=-(AA|EF))" # e.g. 'IP' in 'IP-AA-001'
       ) |>
-        stringr::str_to_lower()
+        stringr::str_to_lower(),
+      activity_type_name = dplyr::case_match(
+        activity_type,
+        "ip" ~ "Inpatients",
+        "op" ~ "Outpatients",
+        "ae" ~ "Accident & Emergency"
+      )
     ) |>
-    dplyr::nest_by(.data$category) |>
-    tibble::deframe()
+    dplyr::left_join(categories, by = "strategy") |>
+    dplyr::select(
+      # Order columns from broadest to narrowest
+      tidyselect::starts_with("activity_type"),
+      tidyselect::starts_with("category"),
+      tidyselect::starts_with("strategy")
+    )
 }
 
 #' Select Strategy Server
@@ -70,7 +87,7 @@ mod_select_strategy_get_strategies <- function() {
 #' @noRd
 mod_select_strategy_server <- function(id) {
   # load static data items
-  strategies <- mod_select_strategy_get_strategies()
+  strategies_lookup <- mod_select_strategy_get_strategies()
 
   # return the shiny module
   shiny::moduleServer(id, function(input, output, session) {
@@ -79,14 +96,39 @@ mod_select_strategy_server <- function(id) {
       input$strategy_activity_type_select
     })
 
-    shiny::observe({
-      category <- shiny::req(selected_activity_type())
+    selected_category <- shiny::reactive({
+      shiny::req(input$strategy_category_select)
+      input$strategy_category_select
+    })
 
-      strategy_choices <- strategies[[category]] |>
-        dplyr::select("name", "strategy") |>
+    observe({
+      activity_type <- req(selected_activity_type())
+
+      category_choices <- strategies_lookup |>
+        dplyr::filter(.data$activity_type == .env$activity_type) |>
+        dplyr::distinct(category_name, category) |>
         tibble::deframe()
 
-      shiny::updateSelectInput(
+      updateSelectInput(
+        session,
+        "strategy_category_select",
+        choices = category_choices
+      )
+    })
+
+    observe({
+      activity_type <- req(selected_activity_type())
+      category <- req(selected_category())
+
+      strategy_choices <- strategies_lookup |>
+        dplyr::filter(
+          .data$activity_type == .env$activity_type,
+          .data$category == .env$category
+        ) |>
+        dplyr::select(strategy_name, strategy) |>
+        tibble::deframe()
+
+      updateSelectInput(
         session,
         "strategy_select",
         choices = strategy_choices
